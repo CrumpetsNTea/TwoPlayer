446730e4c4a4682124e7d854301696623c9a6967
CrumpetsNTea <t.botell@gmail.com>
2022-01-03 17:51:21 -0500

    Add require Player, use player in the functionality of Question so now it checks if turn is falsy and asks the corresponding truthy turn player the question

--- question.rb
+++ question.rb
@@ -1,3 +1,5 @@
+require './player.rb'
+
 class Question
   attr_reader :new_question, :answer
   
@@ -6,10 +8,17 @@ def initialize
   num2 = rand(1..10)
   @question = "What is #{num1} + #{num2}?"
   @answer = num1 + num2
+ @player1 = Player.new("P1", true)
+ @player2 = Player.new("P2", false)
+
 end
 
 def make_question
-  puts "#{@question}"
+   if @player1.turn
+  puts "#{@player1.name} : #{@question}"
+   elsif @player2.turn
+  puts "#{@player2.name} : #{@question}"
+  end
   if gets.chomp.to_i == @answer
         puts "correct!"
       else
