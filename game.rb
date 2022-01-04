f5dcd4b4b44bad4a5b71cf60836d65f92dd502e0
CrumpetsNTea <t.botell@gmail.com>
2022-01-03 19:41:16 -0500

    Add functionality to check for a winner after each answer. Game is now fully functional

--- game.rb
+++ game.rb
@@ -18,7 +18,7 @@ def make_question
  puts "#{@player1.name} : #{@new_question.question}"
   elsif @player2.turn
  puts "#{@player2.name} : #{@new_question.question}"
- end
+  end
 end
 
 def check_answer
@@ -33,7 +33,7 @@ if gets.chomp.to_i == @new_question.answer
     @player2.turn = false
   end
   puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
-  next_turn
+  check_winner
 else
   if @player1.turn
     @player1.lives -= 1
@@ -44,11 +44,20 @@ else
     @player2.lives -= 1
     @player1.turn = true
     @player2.turn = false
-end
+  end
 puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
-next_turn
+check_winner
 end
 end
 
+def check_winner
+if @player1.lives == 0
+  puts "Game Over\n#{@player2.name} wins with a final score of: #{@player2.lives}/3 "
+elsif @player2.lives == 0
+  puts "Game Over\n#{@player1.name} wins with a final score of: #{@player1.lives}/3 "
+else
+  next_turn
+end
+end
 end
 
